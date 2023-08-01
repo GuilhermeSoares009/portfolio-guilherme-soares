5a0d44cb0791dcf45dd4e982dfa4b8a1d46c37d8
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-08-01 18:26:58 -0300

    Habilidades + portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,7 +56,20 @@ skillsHeader.forEach((el) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
